e32abb4b9eb78b812b32cc711cb8f06f8246e201
Greg Palmer <greg@gpalmer.me>
2020-12-22 20:48:29 -0800

    Login into account info basics

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
 console.log('AN/UG Financial')
 
-const logo = document.getElementById('logo');
-const loginContainer = document.getElementById('loginContainer')
+// login elements
+const loginLogo = document.getElementById('loginLogo');
 const loginFormUsername = document.getElementById('loginFormName');
 const loginFormPassword = document.getElementById('loginFormPassword');
 const loginForm = document.getElementById('loginForm');
 
+// account elements
+const mainLogo = document.getElementById('mainLogo');
+const welcomeHeader = document.getElementById('welcomeHeader');
+const currentBalance = document.getElementById('currentBalance');
+
 loginForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log('Login submit!')
     const username = loginFormUsername.value;
     const pass = loginFormPassword.value;
-    console.log('Username:', username);
-    console.log('Password:', pass);
     const currentUser = validateLogin(username, pass);
     if (!currentUser) {
         window.alert('Invalid username or password');
@@ -25,31 +27,31 @@ const USERS = {
         user: 'admin',
         pass: 'admin',
         displayName: 'Admin',
-        currentBalance: '$1,000,000'
+        currentBalance: '$1,000,000.00'
     },
     tac: {
         user: 'tac',
         pass: '10012009',
         displayName: 'Ty',
-        currentBalance: '$900'
+        currentBalance: '$900.00'
     },
     moc: {
         user: 'moc',
         pass: '02162013',
         displayName: 'Makenzie',
-        currentBalance: '$900'
+        currentBalance: '$900.00'
     },
     wnw: {
         user: 'wnw',
         pass: '05012012',
         displayName: 'Whitney',
-        currentBalance: '$900'
+        currentBalance: '$900.00'
     },
     wew: {
         user: 'wew',
         pass: '05022014',
         displayName: 'Weslyn',
-        currentBalance: '$900'
+        currentBalance: '$900.00'
     }
 };
 
@@ -63,6 +65,27 @@ function validateLogin(username='', pass='') {
 }
 
 function loadAccount(currentUser) {
-    logo.classList.add('move-left');
-    loginContainer.classList.add('move-right');
+    welcomeHeader.textContent = `${getWelcomeMessage()} ${currentUser.displayName}`;
+    currentBalance.textContent = currentUser.currentBalance;
+
+    loginLogo.classList.add('move-left');
+    loginForm.classList.add('move-right');
+    setTimeout(() => {
+        mainLogo.classList.remove('hidden');
+        accountContainer.classList.add('show');
+    }, 700);
+}
+
+function getWelcomeMessage () {
+    const d = new Date();
+    const hours = d.getHours();
+    if (hours < 12) {
+        return 'Good Morning,';
+    } else if (hours >= 12 && hours < 17) {
+        return 'Good Afternoon,';
+    } else if (hours >= 17 && hours <=24) {
+        return 'Good Evening,'
+    } else {
+        return 'Hello,'
+    }
 }
