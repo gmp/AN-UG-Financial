e88baa27367a32156271797b0dc736d1e12649b7
Greg Palmer <greg@gmp.dev>
2021-11-24 10:59:21 -0800

    Use Next <Link> for routing

--- web/components/_layout.tsx
+++ web/components/_layout.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react'
 import Head from 'next/head'
+import Link from 'next/link'
 
 type LayoutProps = {
   children: ReactElement
@@ -14,9 +15,11 @@ export default function Layout({ children }: LayoutProps) {
       </Head>
       <div className='flex flex-col items-center justify-center min-h-screen bg-gradient-to-t from-gray-600 to-gray-400'>
         <header className='px-6 mb-4'>
-          <a href='/' className='block'>
-            <img src='https://gmp.imgix.net/ANUG_logo_transparent.png' />
-          </a>
+          <Link href='/'>
+            <a className='block'>
+              <img src='https://gmp.imgix.net/ANUG_logo_transparent.png' />
+            </a>
+          </Link>
         </header>
         <main className='container'>{children}</main>
         <footer className='fixed bottom-2 font-light text-xs text-gray-300'>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,13 +1,18 @@
+import Link from 'next/link'
 import type { NextPage, GetServerSideProps } from 'next'
 
 const Home: NextPage = () => {
   return (
     <div className='flex flex-col items-center justify-center space-y-3 font-light text-gray-300'>
       <div className='w-1/2 text-center bg-red-900 transform skew-x-37'>
-        <a href='/login' className='block p-2 transform -skew-x-37'>login</a>
+        <Link href='/login'>
+          <a className='block p-2 transform -skew-x-37'>login</a>
+        </Link>
       </div>
       <div className='w-1/2 text-center bg-red-900 transform skew-x-37'>
-        <a href='/signup' className='block p-2 transform -skew-x-37'>signup</a>
+        <Link href='/signup'>
+          <a href='/signup' className='block p-2 transform -skew-x-37'>signup</a>
+        </Link>
       </div>
     </div>
   )
