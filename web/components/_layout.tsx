5c9eba4c771b30646e07671bb193687579c5cb8a
Greg Palmer <greg@gmp.dev>
2021-11-24 10:10:50 -0800

    Add layout bg gradient and copyright footer

--- web/components/_layout.tsx
+++ web/components/_layout.tsx
@@ -12,11 +12,14 @@ export default function Layout({ children }: LayoutProps) {
         <title>AN/UG Financial</title>
         <link rel='icon' href='https://gmp.imgix.net/favicon.png' />
       </Head>
-      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-500'>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gradient-to-t from-gray-600 to-gray-400'>
         <header className='px-6 mb-4'>
           <img src='https://gmp.imgix.net/ANUG_logo_transparent.png' />
         </header>
         <main className='container'>{children}</main>
+        <footer className='fixed bottom-2 font-light text-xs text-gray-300'>
+          <p>&copy;2021 | AN/UG Financial</p>
+        </footer>
       </div>
     </>
   )
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -4,10 +4,10 @@ const Home: NextPage = () => {
   return (
     <div className='flex flex-col items-center justify-center space-y-3 font-light text-gray-300'>
       <div className='w-1/2 text-center bg-red-900 transform skew-x-37'>
-        <a href='/login' className='block p-2 transform -skew-x-37'>Login</a>
+        <a href='/login' className='block p-2 transform -skew-x-37'>login</a>
       </div>
       <div className='w-1/2 text-center bg-red-900 transform skew-x-37'>
-        <a href='/signup' className='block p-2 transform -skew-x-37'>Signup</a>
+        <a href='/signup' className='block p-2 transform -skew-x-37'>signup</a>
       </div>
     </div>
   )
